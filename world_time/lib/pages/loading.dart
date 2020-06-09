f3924836759c066d989bb4a80a6f4321fee0f8e8
nnarziev95 <nnarziev@gmail.com>
2020-06-09 21:08:02 +0900

    created a WorldTime class and used it's instance to show on UI

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:worldtime/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,24 +7,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+  String time = 'loading';
 
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Seoul', flag: 'korea.png', url: 'Asia/Seoul');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setupWorldTime();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+          child: Text(time)
+      ),
     );
   }
 }
